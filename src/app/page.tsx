be33a5d7c6f81d0fde9ed05d382834065f40fb6c
rehiiii <rehansahab88888@gmail.com>
2026-09-07 00:41:43 +0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '32px' }}>🎉 LIVE! My Whop App Works!</h1>
+      <p>Your build is fixed and homepage is here!</p>
+      <a href="https://vercel.com" style={{ color: 'blue' }}>Powered by Vercel</a>
+    </div>
+  );
+}
